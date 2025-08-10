88cb87d2e2ac5c6760122472021f8f62608ff87d
Kevin A. Brown <kevin.brown.jm@gmail.com>
2025-08-10 13:07:42 -0500

    Allow changing simulation end time in exec.sh

--- multiflow/exec.sh
+++ multiflow/exec.sh
@@ -2,7 +2,8 @@
     --workload_conf_file=../conf/work.load          \
     --alloc_file=../conf/alloc.conf         \
     --lp-io-dir=riodir --lp-io-use-suffix=1         \
-    --max_gen_data=100000000 --payload_sz=64        \
+    --payload_sz=64        \
+    --end=30000     \
     --workload_period_file=period.file  \
     -- ../conf/modsim-dfdally72-min.conf \
     &>> 3.flows.out
